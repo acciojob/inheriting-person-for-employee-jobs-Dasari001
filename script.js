dd3d22984f1ff5a2c2ceab8a8a8bbaac821db29a
Dasari001 <107811202+Dasari001@users.noreply.github.com>
2023-04-09 09:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	Person(name,age){
+		greet(){
+			console.log(`Hello, my name is ${this.name} , I am ${this.age} years old`)
+		}
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee extends Person(name, age, jobTitle) {
+	Employee(name,age,jobTitle){
+		super()
+		jobGreet(){
+			console.log(`Hello, my name is ${this.name} , I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+		}
+	}
+}
+
+var employee1 = new Employee('sai',23,'instructor');
 
 // Do not change code below this line
 window.Person = Person;
